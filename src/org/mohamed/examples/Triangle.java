536b537fb85415cfb90c034e53fdd7755137ec42
MohamedZajith <mohamedzajith@gmail.com>
2015-10-15 14:57:30 +0530

    used java collections(List)

--- src/org/mohamed/examples/Triangle.java
+++ src/org/mohamed/examples/Triangle.java
@@ -1,41 +1,25 @@
 package org.mohamed.examples;
 
+import java.util.List;
+
 /**
  * Created by dba on 10/13/15.
  */
 public class Triangle {
 
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    private List<Point> points;
 
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
     public void draw(){
-        System.out.println("PointA = ( "+ getPointA().getX() + " , " + getPointA().getY() + " ) ");
-        System.out.println("PointB = ( "+ getPointB().getX() + " , " + getPointB().getY() + " ) ");
-        System.out.println("PointC = ( "+ getPointC().getX() + " , " + getPointC().getY() + " ) ");
+        for (Point  point : points){
+            System.out.println("Point = ( "+ point.getX() + " , " + point.getY() + " ) ");
+        }
     }
 }
